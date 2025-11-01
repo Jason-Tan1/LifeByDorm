5c78e9893c3625620f85915fe2e95c38555f5fd1
Jason Tan <jatan2745@gmail.com>
2025-11-01 01:22:14 -0400

    All Pages Linked Together

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home.tsx';
+import AboutMe from './pages/aboutme.tsx';
+import ContactMe from './pages/contactme.tsx';
+import Login from './pages/login.tsx';
+import University from './universitys.tsx';
+import Dorms from './dorms.tsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/contactme" element={<ContactMe />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/university" element={<University />} />
+        <Route path="/dorms" element={<Dorms />} />
       </Routes>
     </Router>
   )
--- my-app/src/home.tsx
+++ my-app/src/home.tsx
@@ -1,49 +1,60 @@
 import React from 'react';
 import './Home.css'; // Make sure to import the CSS file
+import './navbar.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="home">
       <div className="homeContainer">
          {/* Navigation Bar */}
-        <div className="home_navbar">
+        <div className="navbar">
             {/* Navigation Bar Logo */}
           <div className="navbar_logo">
-            <a>LifeByDorm</a>
+            <Link to ="/">  
+              <a>LifeByDorm</a>
+            </Link>
           </div>
             {/* Navigation Bar Buttons */}
           <div className="navbar_actions">
             <div className="navbar_aboutme">
-              <button>About Me</button>
+              <Link to="/aboutme">
+                <button>About Me</button>
+              </Link>
             </div>
             <div className="navbar_contactme">
-              <button>Contact Me</button>
+              <Link to="/contactme"> 
+                <button>Contact Me</button>
+              </Link>
             </div>
             <div className="navbar_login">
-              <button>Log In</button>
+              <Link to="/login">
+                <button>Log In</button>
+              </Link>
             </div>
           </div>
         </div>
-        {/* Search Bar */}
-        
+        {/* Search Bar (Seperate File) */}
+          <h2> (Search Bar Goes Here)</h2>
         {/* Quick Links to Universities */}
-        <button>
-          University of Toronto
-        </button>
-        <button>
-          Western University
-        </button>
-        <button>
-          York University
-        </button>
-        <button>
-          McMaster University
-        </button>
-        <button>
-          McGill University
-        </button>
+        <div className = "quicklinks"> 
+          <h2> Quick Links: </h2>
+          <Link to ="/university">  
+            <button>
+              University of Toronto
+            </button>
+            <button>
+              Western University
+            </button>
+            <button>
+              York University
+            </button>
+          </Link>
+        </div>
         {/* Leaderboard */}
-
+        <div className="leaderboard">
+          <h2> Leaderboard: </h2>
+        </div>
       </div>
     </div>
   );
