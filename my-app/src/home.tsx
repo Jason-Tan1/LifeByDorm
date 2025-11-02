491f41b0a12a2c03295838117795ca2f085174d8
Jason Tan <jatan2745@gmail.com>
2025-11-01 23:27:51 -0400

    Home Page Styling

--- my-app/src/home.tsx
+++ my-app/src/home.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import './Home.css'; // Make sure to import the CSS file
 import { Link } from 'react-router-dom';
-import NavBar from './navbar.tsx';
-import SearchBar from './searchbar.tsx';
+import NavBar from './navbar.tsx'; // Assuming you have this component
+import SearchBar from './searchbar.tsx'; // Assuming you have this component
 
 function Home() {
   return (
-    <div className="home">
+    <div className = "home"> 
       <NavBar />
-      <div className="homeContent">
-        {/* Search Bar Placeholder */}
-        <div className = "home_searchbar"> 
-           <h2> Real Canadian University Dorm Reviews from Previous Students!</h2>
-           <SearchBar />
-        </div>
-        {/* Quick Links to Universities */}
-        <div className="quicklinks">
-          <h2 className="section-title">Quick Links:</h2>
-          <div className="quicklinks-buttons">
-            <Link to="/university">
-              <button>University of Toronto</button>
-            </Link>
-            <Link to="/university">
-              <button>Western University</button>
-            </Link>
-            <Link to="/university">
-              <button>York University</button>
-            </Link>
+      <div className="home-container">
+        <div className="home-content">
+          <div className="home-section">
+            <h1>
+              Your Honest Look
+              <br />
+              at Canadian University
+              <br />
+              Dorms Straight from
+              <br />
+              Real Students
+            </h1>
+            <SearchBar />
+          </div>
+
+          <div className="quicklinks-section">
+            <h2>Quick Links:</h2>
+            <div className="quicklinks-buttons">
+              <Link to="/university">
+                <button>University of Toronto</button>
+              </Link>
+              <Link to="/university">
+                <button>Western University</button>
+              </Link>
+              <Link to="/university">
+                <button>York University</button>
+              </Link>
+            </div>
           </div>
         </div>
-
-        {/* Featured Dorms */}
-        <div className="preview">
-          <h2 className="section-title">Preview of Dorms:</h2>
-        </div>
-
-        {/* Footer */}
-
       </div>
     </div>
   );
