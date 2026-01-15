0ef0421a9c83a372098b75c80ea59106747eac6a
Jason Tan <jatan2745@gmail.com>
2026-01-15 00:48:32 -0500

    Configure production env variables

--- my-app/src/NavBarPages/account.tsx
+++ my-app/src/NavBarPages/account.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import NavBar from './navbar.tsx';
 import Footer from '../homepage/footer.tsx';
--- my-app/src/NavBarPages/navbar.tsx
+++ my-app/src/NavBarPages/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import LoginModal from './login';
